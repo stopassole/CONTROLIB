659fcce8ea2f27a546756b5dba5f9dd3fbee28c5
AlissonStopassole <alissonstopassole@hotmail.com>
2018-08-21 14:19:08 -0300

    criado sistema para backup quando usuario deslogar

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -15,56 +15,72 @@ public class DashboardController {
 	private Button btnLivros;
 	@FXML
 	private Button btnUsuarios;
-	
+
 	@FXML
 	private void sair() throws Exception {
+		criarBackupBanco();
 		Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
 		Scene scene = new Scene(root);
 		Inicio.myStage.setScene(scene);
 	}
-	
+
 	@FXML
 	private void menuUsuarios() throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/view/ListUsuario.fxml"));
 		Scene scene = new Scene(root);
 		Inicio.myStage.setScene(scene);
 	}
-	
+
 	@FXML
 	private void menuEmprestimos() throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/view/ListEmprestimos.fxml"));
 		Scene scene = new Scene(root);
 		Inicio.myStage.setScene(scene);
 	}
-	
+
 	@FXML
 	private void menuLivros() throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/view/ListLivros.fxml"));
 		Scene scene = new Scene(root);
 		Inicio.myStage.setScene(scene);
 	}
-	
+
 	public void enterPressedLivros(KeyEvent e) throws Exception {
-        if (e.getCode().toString().equals("ENTER")) {
-        	menuLivros();
-        }
-    }
-	
+		if (e.getCode().toString().equals("ENTER")) {
+			menuLivros();
+		}
+	}
+
 	public void enterPressedEmprestimos(KeyEvent e) throws Exception {
-        if (e.getCode().toString().equals("ENTER")) {
-        	menuEmprestimos();
-        }
-    }
+		if (e.getCode().toString().equals("ENTER")) {
+			menuEmprestimos();
+		}
+	}
 
 	public void enterPressedUsuarios(KeyEvent e) throws Exception {
-        if (e.getCode().toString().equals("ENTER")) {
-        	menuUsuarios();
-        }
-    }
-	
+		if (e.getCode().toString().equals("ENTER")) {
+			menuUsuarios();
+		}
+	}
+
 	public void enterPressedSair(KeyEvent e) throws Exception {
-        if (e.getCode().toString().equals("ENTER")) {
-        	sair();
-        }
-    }
+		if (e.getCode().toString().equals("ENTER")) {
+			sair();
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public boolean criarBackupBanco() {
+		 {
+	         try{  
+	            ProcessBuilder pb = new ProcessBuilder("C:\\Program Files\\PostgreSQL\\10\\bin\\pg_dump.exe ", "-h", "localhost", "-p", "5432","-U", "postgres", "-F", "c", "-b", "-v" ,"-f", "C:\\Controlib\\bkp.backup", "controlib"); 
+	            pb.environment().put("PGPASSWORD", "postgres");  
+	            pb.redirectErrorStream(true);  
+	            Process p = pb.start();
+	        }catch(Exception ex){  
+	            return false;
+	        }
+	        return true;
+	    }
+	}
 }
